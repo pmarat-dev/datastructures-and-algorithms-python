ca0e86bda5d43709c0b830a6b42487c4608c565f
Piotr <pmarat.dev@gmail.com>
2023-11-29 01:17:14 +0100

    Added is_subset.py.

---
+++ src/algorithms/hashing/is_subset.py
@@ -0,0 +1,25 @@
+def is_subset(arr1, m, arr2, n):
+    hashset = set()
+
+    for i in range(0, m):
+        hashset.add(arr1[i])
+
+    for i in range(0, n):
+        if arr2[i] in hashset:
+            continue
+        else:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    arr1 = [11, 1, 13, 21, 3, 7]
+    arr2 = [11, 3, 7, 1]
+
+    m = len(arr1)
+    n = len(arr2)
+
+    if is_subset(arr1, m, arr2, n):
+        print("arr2[] is subset of arr1[] ")
+    else:
+        print("arr2[] is not a subset of arr1[] ")
